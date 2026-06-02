e053f43f83eb7f34cc6498e048d7c58d9b416ca4
fdiallo <fallaye.diallo@gmail.com>
2026-06-02 12:39:08 -0400

    Implemented server to test database connection

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require("express")
+const app = express()
+require("dotenv").config()
+
+
+const { connectDB } = require("./utils/connection.js")
+
+connectDB()
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())     // Middleware to parse JSON bodies
+
+// Routes
+
+
+// Port
+const PORT = process.env.PORT
+app.listen(PORT, () => { console.log(`Server listening on Port: ${PORT}`) })
