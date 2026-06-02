85296c324649470c53217f6688052f25cedca051
fdiallo <fallaye.diallo@gmail.com>
2026-06-02 13:25:28 -0400

    Implementd Projet model

---
+++ backend/models/Project.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true
+    },
+    description: {
+        type: String, required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
+    }
+}, {
+    timestamps: true
+});
+
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = { Project };
