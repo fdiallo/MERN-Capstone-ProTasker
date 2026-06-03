6bbe1a425b7bb42517f1008292c23be4f9469a43
fdiallo <fallaye.diallo@gmail.com>
2026-06-03 11:13:25 -0400

    Implemented fetch to rertrieve data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
 import { useState, useEffect } from "react";
 
 function App() {
- 
+  const [token, setToken] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+    const body = {
+      username: e.target[0].value,
+      email: e.target[1].value,
+      password: e.target[2].value,
+    }
+    getData(body)
+  }
+
+  async function getData(body) {
+    
+    const data = await fetch("http://localhost:3000/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const response = await data.json();
+    console.log(response);
+    setToken(response.token)
+    return response;
+  }
+
   return (
     <>
       <h1>Library Frontend</h1>
