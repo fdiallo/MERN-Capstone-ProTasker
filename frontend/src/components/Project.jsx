00ac55d5bdaeb9ad1bab6214cf953a4af6361ef1
fdiallo <fallaye.diallo@gmail.com>
2026-06-03 17:44:45 -0400

    Added Project component

---
+++ frontend/src/components/Project.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Project({ project, checkProject }) {
+    function handleCheck({target}){
+        checkProject(project)
+    }
+    function handleDelete({target}){
+        project.delete = true
+        checkProject(project)
+    }
+    return (
+        <div>
+
+            <h2>Name: {project.name}</h2>
+            <h3>Description: {project.description}</h3>
+            
+            <button onClick={handleDelete}>X</button>
+        </div>
+    )
+}
